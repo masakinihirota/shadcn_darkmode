5cd1ac91d2c67d400d6f1a500036f0e0c782b8f4
masakinihirota <masakinihirota@gmail.com>
2023-07-27 22:05:13 +0900

    🐛 update page.tsx and layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import React from "react"
+
 import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
